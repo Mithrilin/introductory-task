04fcf7fd6d41fbff4f9ac4099efea9c2b62b8a6b
Roman <romanukyan86@yandex.ru>
2024-09-10 19:09:50 +0300

    feat: added companyToCompanyDto in CompanyMapper;

--- company-service/src/main/java/tech/fivedhub/companyservice/mapper/CompanyMapper.java
+++ company-service/src/main/java/tech/fivedhub/companyservice/mapper/CompanyMapper.java
@@ -1,7 +1,16 @@
 package tech.fivedhub.companyservice.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import tech.fivedhub.companyservice.dto.CompanyDto;
+import tech.fivedhub.companyservice.dto.UserDto;
+import tech.fivedhub.companyservice.model.Company;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CompanyMapper {
+
+    @Mapping(source = "employeesList", target = "employees")
+    CompanyDto companyToCompanyDto(Company company, List<UserDto> employeesList);
 }
