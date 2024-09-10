33314ecbe1f903b74a77fafc1423e1e8f883c463
Roman <romanukyan86@yandex.ru>
2024-09-11 00:00:47 +0300

    feat: added class ConfigServerApp;

---
+++ config-server/src/test/java/tech/fivedhub/configservice/ConfigServerAppTest.java
@@ -0,0 +1,10 @@
+package tech.fivedhub.configservice;
+
+import org.junit.jupiter.api.Test;
+
+class ConfigServerAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ discovery-server/src/test/java/tech/fivedhub/discoveryserver/DiscoveryServerAppTest.java
@@ -0,0 +1,10 @@
+package tech.fivedhub.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+
+class DiscoveryServerAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
