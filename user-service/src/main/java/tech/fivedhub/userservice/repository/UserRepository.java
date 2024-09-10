0e25e03781fa311b5469622a45fb1cc6580220fe
Roman <romanukyan86@yandex.ru>
2024-09-10 16:33:35 +0300

    feat: added findByLastName in UserRepository;

--- user-service/src/main/java/tech/fivedhub/userservice/repository/UserRepository.java
+++ user-service/src/main/java/tech/fivedhub/userservice/repository/UserRepository.java
@@ -3,5 +3,9 @@ package tech.fivedhub.userservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.fivedhub.userservice.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLastName(String lastName);
 }
