5e3fb4042ac7e881893907c01b39cb5c53ad6390
Roman <romanukyan86@yandex.ru>
2024-09-10 18:46:12 +0300

    feat: added class UserDto;

---
+++ company-service/src/main/java/tech/fivedhub/companyservice/dto/UserDto.java
@@ -0,0 +1,12 @@
+package tech.fivedhub.companyservice.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
