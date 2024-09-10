f6beb61a17dc2f406002fbe73ebd470622081b43
Roman <romanukyan86@yandex.ru>
2024-09-10 19:17:07 +0300

    feat: added findByName in CompanyRepository;

--- company-service/src/main/java/tech/fivedhub/companyservice/repository/CompanyRepository.java
+++ company-service/src/main/java/tech/fivedhub/companyservice/repository/CompanyRepository.java
@@ -3,5 +3,9 @@ package tech.fivedhub.companyservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.fivedhub.companyservice.model.Company;
 
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    Optional<Company> findByName(String name);
 }
