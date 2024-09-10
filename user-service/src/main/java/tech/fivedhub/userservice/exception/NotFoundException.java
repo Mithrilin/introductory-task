2411a1bd9a8699283bd6e89723c6a60af937a2df
Roman <romanukyan86@yandex.ru>
2024-09-10 16:23:18 +0300

    feat: added class NotFoundException;

---
+++ user-service/src/main/java/tech/fivedhub/userservice/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package tech.fivedhub.userservice.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
