0cb8fe6e34a39bdd93cc65ce386afcb75c1dcdea
Roman <romanukyan86@yandex.ru>
2024-09-10 16:36:43 +0300

    feat: added class ErrorHandler;

---
+++ user-service/src/main/java/tech/fivedhub/userservice/exception/ErrorHandler.java
@@ -0,0 +1,28 @@
+package tech.fivedhub.userservice.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handleNotFoundException(final NotFoundException e) {
+        log.error("Status 404 Not Found received. {}", e.getMessage(), e);
+        return Map.of("errorMessage", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleAllException(final Exception e) {
+        log.error("Status 500 Internal Server Error received. {}", e.getMessage(), e);
+        return Map.of("errorMessage", e.getMessage());
+    }
+}
