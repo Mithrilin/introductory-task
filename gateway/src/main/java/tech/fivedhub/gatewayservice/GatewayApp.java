ee771e8dfa481d82776877f7ed030c38b2271579
Roman <romanukyan86@yandex.ru>
2024-09-11 14:55:01 +0300

    feat: added GatewayApp.java;

---
+++ gateway/src/main/java/tech/fivedhub/gatewayservice/GatewayApp.java
@@ -0,0 +1,12 @@
+package tech.fivedhub.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+}
